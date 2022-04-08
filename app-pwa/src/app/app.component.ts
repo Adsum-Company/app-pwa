2aac699fe9193d8e7dd0e1de7f86e1396b639fa0
jmcazarez <63732249+jmcazarez@users.noreply.github.com>
2022-04-08 12:02:36 -0600

    ...

---
+++ app-pwa/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'app-pwa';
+
+  mostrar() {
+    Swal.fire('Bienvenido a la PWA');
+  }
+
+}
